1feccd1b3fbce9e4b3e55a73406adbb0c7a096c0
ilutsav <ilutsav@gmail.com>
2023-07-12 02:37:55 +0545

    adding location time and time

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/instance_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:weatherapp/controller/global_controller.dart';
+import 'package:weatherapp/widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +13,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //call
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(() => globalController.checkLoading().isTrue
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  const HeaderWidget(),
+                ],
+              )),
+      ),
+    );
   }
 }
